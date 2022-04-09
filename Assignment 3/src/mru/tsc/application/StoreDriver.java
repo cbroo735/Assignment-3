60fc1bfe396197ea33c9202a4f83cf5c44a35cd9
Colin Brookes <cbroo735@mtroyal.ca>
2022-04-08 23:17:19 -0600

    StoreDriver

--- Assignment 3/src/mru/tsc/application/StoreDriver.java
+++ Assignment 3/src/mru/tsc/application/StoreDriver.java
@@ -1,17 +1,26 @@
 package mru.tsc.application;
 
+import javafx.stage.Stage;
 import mru.tsc.controller.StoreController;
 import mru.tsc.view.Menus;
+import mru.tsc.view.myGUI;
 
 /**
  * This is a class to call the project by calling the StoreController and Menus constructors
  */
 public class StoreDriver {
 	
-	public static void main(String[] args) throws Exception {
+	public void start (Stage primaryStage) throws Exception {
 		
-		new StoreController();
+		StoreController s = new StoreController();
+		myGUI gui = new myGUI(primaryStage, s);
 		new Menus();
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
